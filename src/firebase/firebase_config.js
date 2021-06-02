a96654443236743c519d4630f79c9bc3fa0c3790
aditya6521 <adityasinghr15@gmail.com>
2021-06-02 22:51:00 +0530

    feat firebase

---
+++ src/firebase/firebase_config.js
@@ -0,0 +1,20 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+
+
+var firebaseConfig = {
+    apiKey: "AIzaSyCiicHudEYm28kYcuzZZdVq85BQSqHrE4w",
+    authDomain: "v1-mgmt.firebaseapp.com",
+    projectId: "v1-mgmt",
+    storageBucket: "v1-mgmt.appspot.com",
+    messagingSenderId: "95708668117",
+    appId: "1:95708668117:web:04d4454af90ba2c43e825c"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.firestore().settings({timestampsInSnapshots:true});
+
+  export default firebase;
+

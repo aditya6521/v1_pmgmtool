0c0e37b7d34e3d390e8fa382bae3eee8265e777e
aditya6521 <adityasinghr15@gmail.com>
2021-06-13 01:24:31 +0530

    Navbar routing

--- src/comp/auth/signin.js
+++ src/comp/auth/signin.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 import { SignIn } from '../../store/action/authActions';
 
 
@@ -23,7 +24,8 @@ class signin extends Component {
 
 
     render() {
-        const {authError}=this.props;
+        const {authError, auth}=this.props;
+        if(auth.uid) return <Redirect to="/"/>
         return (
             <div className="container">
                 <form onSubmit={this.handleSubmit}  className="white">
@@ -58,7 +60,8 @@ class signin extends Component {
 
 const mapStateToProps=(state)=>{
     return{
-        authError:state.auth.authError
+        authError:state.auth.authError,
+        auth:state.firebase.auth
     }
 }
 
--- src/comp/project/projectDetails.js
+++ src/comp/project/projectDetails.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux';
 import { firestoreConnect } from 'react-redux-firebase';
+import { Redirect } from 'react-router-dom';
 import { compose } from 'redux';
 
 
@@ -8,7 +9,8 @@ import { compose } from 'redux';
 function projectDetails(props) {
     //const id=props.match.params.id;
     //console.log(props);
-    const {project}=props;
+    const {project,auth}=props;
+    if(!auth.uid) return <Redirect to="/signin"/>
     if(project){
     return(
         <div className="container section project-details">
@@ -44,7 +46,8 @@ const mapStateToProps=(state,ownProps)=>{
     const projects=state.firestore.data.projects;
     const project=projects ? projects[id]:null
     return{
-        project:project
+        project:project,
+        auth:state.firebase.auth
     }
 }
 
